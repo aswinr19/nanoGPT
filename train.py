dc27825b8af26997abe34e4e2ae97599d802766e
aswinr <rashwin@zohotrainees.com>
2024-12-27 09:34:25 +0530

    minor changes

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ n_embd = 64
 dropout = 0.0
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 100
+max_iters = 5000
 lr_decay_iters = 100 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
